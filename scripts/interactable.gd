15b13aeec533491aa44b3e3db2f5f71b582708d6
MarcusLages <marcusvlages@gmail.com>
2026-06-02 13:57:08 -0700

    Criei nodes interagiveis que tambem podem ser arrastados

---
+++ scripts/interactable.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+## ! Using Drag and Drop plugin, but it might need adjustments later
+
+signal interacted
+
+@onready var def_size: Vector2 = self.scale
+
+var HOVER_ZOOM_SIZE := Vector2(1.25, 1.25)
+
+func _on_mouse_entered() -> void:
+    if $Draggable.state == $Draggable.DRAGGABLE_STATE.IDLE:
+        self.scale = HOVER_ZOOM_SIZE
+
+        if Input.is_action_just_pressed("mouse_interact"):
+            interacted.emit()
+
+func _on_mouse_exited() -> void:
+    if $Draggable.state == $Draggable.DRAGGABLE_STATE.IDLE:
+        self.scale = def_size
