5e41e94a173d1960c5d02cc8ef9c53adb16c3345
MarcusLages <marcusvlages@gmail.com>
2026-06-08 19:14:44 -0700

    Changed record cleaning algorithm in DayManager and added better handling of fixed item managers

--- scripts/day_manager.gd
+++ scripts/day_manager.gd
@@ -14,8 +14,7 @@ class_name DayManager
 ## If null, the parent will be checked. If there's no parent, self will be used.
 @export var items_container: Node = null
 
-
-var _curr_record: int = 0
+var _curr_record: int = -1
 
 var fixed_items_manager: InteractableManager = null
 var record_managers: Array[InteractableManager] = []
@@ -27,26 +26,34 @@ func _ready() -> void:
         if not items_container:
             items_container = self
 
-    fixed_items_manager = fixed_items_scene.instantiate() as InteractableManager
-    items_container.add_child(fixed_items_manager)
+    if fixed_items_scene:
+        fixed_items_manager = fixed_items_scene.instantiate() as InteractableManager
+        items_container.add_child(fixed_items_manager)
 
     for rec_scene in record_scenes:
         var rec := rec_scene.instantiate() as InteractableManager
         record_managers.append(rec)
 
 
-func clean_scene():
-    for item in items_container.get_children():
-        if item != fixed_items_manager:
-            item.queue_free()
+func clean_record_scene(clean_fixed: bool = false):
+    if _curr_record < 0 or _curr_record >= record_managers.size():
+        for manager in record_managers:
+            items_container.remove_child(manager)
+    else:
+        items_container.remove_child(record_managers[_curr_record])
+    
+    if fixed_items_manager:
+        # This is just used to make sure all items are enabled again
+        fixed_items_manager.change_state(false, false)
 
-    fixed_items_manager.change_state(false, false)
+        if clean_fixed:
+            items_container.remove_child(fixed_items_manager)
 
 
 func load_record(rec_idx: int) -> Error:
-    clean_scene()
+    clean_record_scene()
     
-    if rec_idx >= record_managers.size():
+    if rec_idx < 0 or rec_idx >= record_managers.size():
         return ERR_DOES_NOT_EXIST
 
     _curr_record = rec_idx
