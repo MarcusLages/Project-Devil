23ccbe6abafb2d9d58c7adaf79e5bd65653e76e5
MarcusLages <marcusvlages@gmail.com>
2026-06-11 15:45:26 -0700

    Fixed bug that would cause the same clock clicking noise to repeat instead of alternating between them

--- scripts/items/alarm.gd
+++ scripts/items/alarm.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
-@export var min_ringing_time_in_seconds: float = 50.
 
-@export var max_ringing_time_in_seconds: float = 120.
+@export var min_ringing_time_sec: float = 50.
 
-@export var clicking_interval_in_seconds: float = 1.
+@export var max_ringing_time_sec: float = 120.
+
+@export var clicking_interval_sec: float = 1.
 
 
 var _rng = RandomNumberGenerator.new()
 var _click: int = 0
-
 
 var ringing: bool = false
 
@@ -17,7 +17,7 @@ var ringing: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     _start_alarm_timer()
-    $ClickTimer.start(clicking_interval_in_seconds)
+    $ClickTimer.start(clicking_interval_sec)
 
 
 func _on_interactable_interacted(_from: Area2D) -> void:
@@ -33,6 +33,7 @@ func _on_alarm_timer_timeout() -> void:
 
 
 func _on_click_timer_timeout() -> void:
+    _click += 1
     var sfx: SoundManager.SFX = (SoundManager.SFX.CLOCK_UP 
         if _click % 2 == 0 
         else SoundManager.SFX.CLOCK_DOWN)
@@ -40,5 +41,5 @@ func _on_click_timer_timeout() -> void:
 
 
 func _start_alarm_timer():
-    var sec: float = _rng.randf_range(min_ringing_time_in_seconds, max_ringing_time_in_seconds)
+    var sec: float = _rng.randf_range(min_ringing_time_sec, max_ringing_time_sec)
     $AlarmTimer.start(sec)
