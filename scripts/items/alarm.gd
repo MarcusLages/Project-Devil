408c58b09377a9a572d0c600b3ed6e7bddd4ce23
Dan Isamu <30096102+MisterProjectLC@users.noreply.github.com>
2026-06-13 01:31:31 -0300

    feat: all profile sheets

--- scripts/items/alarm.gd
+++ scripts/items/alarm.gd
@@ -12,30 +12,30 @@ var ringing: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    _start_alarm_timer()
-    $ClickTimer.start(clicking_interval_sec)
+	_start_alarm_timer()
+	$ClickTimer.start(clicking_interval_sec)
 
 
 func _on_interactable_interacted(_from: Area2D) -> void:
-    if ringing:
-        _start_alarm_timer()
-        SoundManager.stop_sfx(SoundManager.SFX.ALARM)
-        ringing = false
+	if ringing:
+		_start_alarm_timer()
+		SoundManager.stop_sfx(SoundManager.SFX.ALARM)
+		ringing = false
 
 
 func _on_alarm_timer_timeout() -> void:
-    ringing = true
-    SoundManager.play_sfx(SoundManager.SFX.ALARM, true)
+	ringing = true
+	SoundManager.play_sfx(SoundManager.SFX.ALARM, true)
 
 
 func _on_click_timer_timeout() -> void:
-    _click += 1
-    var sfx: SoundManager.SFX = (SoundManager.SFX.CLOCK_UP 
-        if _click % 2 == 0 
-        else SoundManager.SFX.CLOCK_DOWN)
-    SoundManager.play_sfx(sfx)
+	_click += 1
+	var sfx: SoundManager.SFX = (SoundManager.SFX.CLOCK_UP 
+		if _click % 2 == 0 
+		else SoundManager.SFX.CLOCK_DOWN)
+	SoundManager.play_sfx(sfx)
 
 
 func _start_alarm_timer():
-    var sec: float = _rng.randf_range(min_ringing_time_sec, max_ringing_time_sec)
-    $AlarmTimer.start(sec)
+	var sec: float = _rng.randf_range(min_ringing_time_sec, max_ringing_time_sec)
+	$AlarmTimer.start(sec)
--- scripts/text_screen.gd
+++ scripts/text_screen.gd
@@ -20,29 +20,29 @@ class_name TextScreen
 var lines: PackedStringArray
 
 func _ready() -> void:
-    if not lines:
-        lines = opening_line.split("\\")
-    animate_lines()
+	if not lines:
+		lines = opening_line.split("\\")
+	animate_lines()
 
 func animate_lines():
-    var tween: Tween
-    for line in lines:
-        var label = Label.new()
-        label.text = line
-        label.modulate.a = 0
-        label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-        label.add_theme_font_override("font", title_font)
-        label.add_theme_font_size_override("font_size", font_size)
-        container.add_child(label)
+	var tween: Tween
+	for line in lines:
+		var label = Label.new()
+		label.text = line
+		label.modulate.a = 0
+		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		label.add_theme_font_override("font", title_font)
+		label.add_theme_font_size_override("font_size", font_size)
+		container.add_child(label)
 
-        tween = create_tween()
-        tween.set_trans(Tween.TRANS_LINEAR)
-        tween.tween_property(label, "modulate:a", 1., fade_in_duration_sec)
-        await tween.finished
-        await get_tree().create_timer(line_wait_sec).timeout
-    
-    # After all lines, fade out
-    tween = create_tween()
-    tween.tween_property(container, "modulate:a", 0., line_wait_sec)
-    await tween.finished
-    get_tree().change_scene_to_packed(change_to_scene)
+		tween = create_tween()
+		tween.set_trans(Tween.TRANS_LINEAR)
+		tween.tween_property(label, "modulate:a", 1., fade_in_duration_sec)
+		await tween.finished
+		await get_tree().create_timer(line_wait_sec).timeout
+	
+	# After all lines, fade out
+	tween = create_tween()
+	tween.tween_property(container, "modulate:a", 0., line_wait_sec)
+	await tween.finished
+	get_tree().change_scene_to_packed(change_to_scene)
