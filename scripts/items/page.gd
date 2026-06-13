2afaaf5b48a420babe2895e16257639edc1d3e19
MarcusLages <marcusvlages@gmail.com>
2026-06-12 20:00:33 -0700

    Implemented book and going to the next page

--- scripts/items/page.gd
+++ scripts/items/page.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+class_name Page
+
 signal next_page
+signal prev_page
 
 
-func _on_button_pressed() -> void:
+func _on_next_button_pressed() -> void:
     next_page.emit()
+
+
+func _on_prev_button_pressed() -> void:
+    prev_page.emit()
