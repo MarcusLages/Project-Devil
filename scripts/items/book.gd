99c10f8cf8d2552fbf6a6e8ba22a2460dbab6215
Dan Isamu <30096102+MisterProjectLC@users.noreply.github.com>
2026-06-13 10:18:59 -0300

    feat: background noises

--- scripts/items/book.gd
+++ scripts/items/book.gd
@@ -3,50 +3,53 @@ extends Area2D
 @export var pages_container: Node2D
 
 @onready var interactable: Interactable = $Interactable
+@onready var page_sfx = $PageSFX
 
 var pages: Array[Page]
 var curr_page: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    if not pages_container:
-        pages_container = get_node_or_null("ZoomedContent")
-    
-    assert(pages_container != null, "Pages container cannot be null")
+	if not pages_container:
+		pages_container = get_node_or_null("ZoomedContent")
+	
+	assert(pages_container != null, "Pages container cannot be null")
 
-    for p in pages_container.get_children():
-        pages.append(p)
-        p.next_page.connect(_on_page_next_page)
-        p.prev_page.connect(_on_page_prev_page)
+	for p in pages_container.get_children():
+		pages.append(p)
+		p.next_page.connect(_on_page_next_page)
+		p.prev_page.connect(_on_page_prev_page)
 
-    # Godot is weird
-    pages.sort_custom(func(a, b): return int(a.name) < int(b.name))
-    for p in pages:
-        p.visible = false
-        print(p.name)
+	# Godot is weird
+	pages.sort_custom(func(a, b): return int(a.name) < int(b.name))
+	for p in pages:
+		p.visible = false
+		print(p.name)
 
-    pages[0].visible = true
+	pages[0].visible = true
 
 
 func _on_page_next_page():
-    if curr_page < pages.size() - 1:
-        pages[curr_page].visible = false
-        curr_page += 1
-        pages[curr_page].visible = true
+	if curr_page < pages.size() - 1:
+		pages[curr_page].visible = false
+		curr_page += 1
+		pages[curr_page].visible = true
+		page_sfx.play()
 
 
 func _on_page_prev_page():
-    if curr_page > 0:
-        pages[curr_page].visible = false
-        curr_page -= 1
-        pages[curr_page].visible = true
+	if curr_page > 0:
+		pages[curr_page].visible = false
+		curr_page -= 1
+		pages[curr_page].visible = true
+		page_sfx.play()
 
 
 func _on_zoomable_zoomed() -> void:
-    interactable.hard_disabled = true
+	interactable.hard_disabled = true
 
 func _on_zoomable_unzoomed() -> void:
-    interactable.hard_disabled = false
-    pages[curr_page].visible = false
-    curr_page = 0
-    pages[curr_page].visible = true
+	interactable.hard_disabled = false
+	pages[curr_page].visible = false
+	curr_page = 0
+	pages[curr_page].visible = true
