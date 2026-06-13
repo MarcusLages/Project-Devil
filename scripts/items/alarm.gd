cf70f3d42be9efd8b148917bad9d9455c7086d2d
MarcusLages <marcusvlages@gmail.com>
2026-06-13 06:21:27 -0700

    Fixed alarm/item bug

--- scripts/items/alarm.gd
+++ scripts/items/alarm.gd
@@ -17,32 +17,32 @@ var ringing: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_start_alarm_timer()
-	$ClickTimer.start(clicking_interval_sec)
+    _start_alarm_timer()
+    $ClickTimer.start(clicking_interval_sec)
 
 
 func _on_interactable_interacted(_from: Area2D) -> void:
-	if ringing:
-		_start_alarm_timer()
-		SoundManager.stop_sfx(SoundManager.SFX.ALARM)
-		ringing = false
-		sprite.texture = phone_off
+    if ringing:
+        _start_alarm_timer()
+        SoundManager.stop_sfx(SoundManager.SFX.ALARM)
+        ringing = false
+        sprite.texture = phone_off
 
 
 func _on_alarm_timer_timeout() -> void:
-	ringing = true
-	sprite.texture = phone_on
-	SoundManager.play_sfx(SoundManager.SFX.ALARM, true)
+    ringing = true
+    sprite.texture = phone_on
+    SoundManager.play_sfx(SoundManager.SFX.ALARM, true)
 
 
 func _on_click_timer_timeout() -> void:
-	_click += 1
-	var sfx: SoundManager.SFX = (SoundManager.SFX.CLOCK_UP 
-		if _click % 2 == 0 
-		else SoundManager.SFX.CLOCK_DOWN)
-	SoundManager.play_sfx(sfx)
+    _click += 1
+    var sfx: SoundManager.SFX = (SoundManager.SFX.CLOCK_UP 
+        if _click % 2 == 0 
+        else SoundManager.SFX.CLOCK_DOWN)
+    SoundManager.play_sfx(sfx)
 
 
 func _start_alarm_timer():
-	var sec: float = _rng.randf_range(min_ringing_time_sec, max_ringing_time_sec)
-	$AlarmTimer.start(sec)
+    var sec: float = _rng.randf_range(min_ringing_time_sec, max_ringing_time_sec)
+    $AlarmTimer.start(sec)
